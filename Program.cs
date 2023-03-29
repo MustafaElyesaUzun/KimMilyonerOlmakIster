a9e8c5aed27d1960a6927c3d3c636e539317477a
musta <musta@DESKTOP-6IO87PH>
2023-03-29 20:22:00 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace KimMilyonerOlmakIster
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
