7decb2b872255cb7345000e0426c6e059cc60a30
MustafaElyesaUzun <mustafa.elyesa.uzun@hotmail.com>
2023-03-29 20:26:47 +0300

    feat: add title competitor and joker information inputs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Kim Milyoner Olmak İster");
+
+            Console.WriteLine("İsim Giriniz: ");
+            string isim = Console.ReadLine();
+            Console.WriteLine("Soyisim Giriniz: ");
+            string soyisim = Console.ReadLine();
+
+            Console.WriteLine("\n1. Telefon Jokerinizi Giriniz: ");
+            string telefonJoker1 = Console.ReadLine();
+            Console.WriteLine("\n2. Telefon Jokerinizi Giriniz: ");
+            string telefoJoker2 = Console.ReadLine();
+            Console.WriteLine("\n3. Telefon Jokerinizi Giriniz: ");
+            string telefonJoker3 = Console.ReadLine();
         }
     }
 }
